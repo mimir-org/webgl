92b91508bc4bfec22e60009f948d80ffdcf215f9
Kai Andre Hovden <kai.hovden@bouvet.no>
2022-02-17 11:16:02 +0100

    Repeating texture

--- js/app.js
+++ js/app.js
@@ -5,6 +5,19 @@ let renderer;
 let scene;
 let mesh;
 
+var boxWidth = 10;
+var boxHeight = 5;
+var boxDepth = 5;
+
+var cameraFov = 35;
+var cameraRad = cameraFov * Math.PI / 180;
+var cameraNear = 0.1;
+var cameraFar = 1000;
+
+var cameraPosX = 0;
+var cameraPosY = 0;
+var cameraPosZ = (boxWidth * boxHeight) / 3;
+
 window.addEventListener("resize", onWindowResize);
 window.addEventListener("mousedown", onMouseDown);
 init();
@@ -28,13 +41,13 @@ function init() {
 
 function createCamera() {
   camera = new THREE.PerspectiveCamera(
-    35, // FOV
+    cameraFov, // FOV
     container.clientWidth / container.clientHeight, // aspect
-    1, // near clipping plane
-    100 // far clipping plane
+    cameraNear, // near clipping plane
+    cameraFar // far clipping plane
   );
 
-  camera.position.set(0, 0, 10);
+  camera.position.set(cameraPosX, cameraPosY, cameraPosZ);
 }
 
 function createControls() {
@@ -42,42 +55,61 @@ function createControls() {
 }
 
 function createLights() {
-  const hemisphereLight = new THREE.HemisphereLight(0xddeeff, 0x200020, 3);
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  const hemisphereLight = new THREE.HemisphereLight(0xddeeff, 0x200020, 6);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
 
-  hemisphereLight.position.set(-50, 2, 4);
-  directionalLight.position.set(-50, 2, 4);
+  hemisphereLight.position.set(0, 0, 0);
+  directionalLight.position.set(0, 0, 10);
 
   scene.add(hemisphereLight);
   scene.add(directionalLight);
 }
 
 function createMeshes() {
+
   const loader = new THREE.TextureLoader();
-  const geometry = new THREE.BoxBufferGeometry(8, 4, 4);
+  const geometry = new THREE.BoxBufferGeometry(boxWidth, boxHeight, boxDepth);
   
   const materials = [
     new THREE.MeshBasicMaterial({
-      map: loader.load("resources/images/platform1.jpg"),side: THREE.BackSide 
+      map: loader.load("resources/images/platform1.jpg"),side: THREE.BackSide //Right
     }),
     new THREE.MeshBasicMaterial({
-      map: loader.load("resources/images/platform2.jpg"),side: THREE.BackSide 
+      map: loader.load("resources/images/platform2.jpg"),side: THREE.BackSide //Left
     }),
     new THREE.MeshBasicMaterial({
-      map: loader.load("resources/images/platform3.jpg"),side: THREE.BackSide 
+      map: loader.load("resources/images/platform3.jpg"),side: THREE.BackSide //Top
     }),
     new THREE.MeshBasicMaterial({
-      map: loader.load("resources/images/platform4.jpg"),side: THREE.BackSide 
+      map: loader.load("resources/images/platform4.jpg"),side: THREE.BackSide //Bottom
     }),
     new THREE.MeshBasicMaterial({
-      map: loader.load("resources/images/platform5.jpg"),side: THREE.BackSide 
+      map: loader.load("resources/images/platform5.jpg"),side: THREE.BackSide //Front
     }),
     new THREE.MeshBasicMaterial({
-      map: loader.load("resources/images/platform6.jpg"),side: THREE.BackSide 
+      map: loader.load("resources/images/platform6.jpg"),side: THREE.BackSide //Back
     }),
   ];
 
-  mesh = new THREE.Mesh(geometry, materials);
+  //Repeating testure START
+  var textureRoof = loader.load( 'resources/images/wall.jpg', function () {
+    textureRoof.wrapS = textureRoof.wrapT = THREE.RepeatWrapping;
+    textureRoof.offset.set( 0, 0 );
+    textureRoof.repeat.set( 1, 1 );
+} );
+
+const materials2 = [
+  new THREE.MeshPhongMaterial( { color: 0xffffff, specular:0x111111, shininess: 10, map: textureRoof, side: THREE.BackSide} ), //Right
+  new THREE.MeshPhongMaterial( { color: 0xffffff, specular:0x111111, shininess: 10, map: textureRoof, side: THREE.BackSide} ), //Left
+  new THREE.MeshPhongMaterial( { color: 0xffffff, specular:0x111111, shininess: 10, map: textureRoof, side: THREE.BackSide} ), //Roof
+  new THREE.MeshPhongMaterial( { color: 0xffffff, specular:0x111111, shininess: 10, map: textureRoof, side: THREE.BackSide} ), //Floor
+  new THREE.MeshPhongMaterial( { color: 0xffffff, specular:0x111111, shininess: 10, map: textureRoof, side: THREE.BackSide} ), //Front
+  new THREE.MeshPhongMaterial( { color: 0xffffff, specular:0x111111, shininess: 10, map: textureRoof, side: THREE.BackSide} ), //Back
+];
+
+//Repeating testure END
+
+  mesh = new THREE.Mesh(geometry, materials2);
   scene.add(mesh);
 }
 
